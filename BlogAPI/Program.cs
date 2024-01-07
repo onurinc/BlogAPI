e1eaa8dfa32e9daa9850a454163b0255e8561ce1
onurinc <onur.i@hotmail.nl>
2024-01-07 19:26:38 +0100

    fix for azure, to work with inmemory db if theres no db

--- BlogAPI/Program.cs
+++ BlogAPI/Program.cs
@@ -7,35 +7,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var conn = builder.Configuration.GetConnectionString("DefaultConnection");
-
+if (string.IsNullOrWhiteSpace(conn))
+{
+    builder.Services.AddDbContext<ApiDbContext>(options => options.UseInMemoryDatabase("ITBlogDb"));
+}
+else
+{
 builder.Services.AddDbContext<ApiDbContext>(options =>
     options.UseNpgsql(conn));
+}
 
-// OLD MSSQL CONNECTION
-//var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
-//var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-//var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-//var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
-//var conn = builder.Configuration.GetConnectionString("DefaultConnection");
-//builder.Services.AddDbContext<ApiDbContext>(options =>
-//    options.UseSqlServer(connectionString));
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-//
+
 app.UseSwagger();
 app.UseSwaggerUI();
 // app.UseHttpsRedirection();
@@ -49,7 +40,8 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<ApiDbContext>();
-    if (context.Database.GetPendingMigrations().Any())
+    
+    if (!context.Database.IsInMemory() && context.Database.GetPendingMigrations().Any())
     {
         context.Database.Migrate();
     }
